d159a62c88b701eed123907b358992b04d15f230
Alexander Oblovatniy <oblovatniy@gmail.com>
2013-10-26 12:18:08 +0300

    Update package info.

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 
 [![Build Status](https://travis-ci.org/IL2HorusTeam/il2ds-log-parser.png?branch=master)](https://travis-ci.org/IL2HorusTeam/il2ds-log-parser)
 [![Coverage Status](https://coveralls.io/repos/IL2HorusTeam/il2ds-log-parser/badge.png)](https://coveralls.io/r/IL2HorusTeam/il2ds-log-parser)
+[![PyPi package](https://badge.fury.io/py/il2ds-log-parser.png)](http://badge.fury.io/py/il2ds-log-parser/)
+[![Downloads](https://pypip.in/d/il2ds-log-parser/badge.png)](https://crate.io/packages/il2ds-log-parser/)
 
 Parse log file of IL-2 FB DS and produce information about events
+
+Installation
+------------
+
+    pip install il2ds-log-parser
--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='il2ds-log-parser',
-    version='0.1.0',
+    version='0.9.0',
     description='Parser of IL-2 FB Dedicated Server events log.',
-    license='MIT',
+    license='BSD License',
     url='https://github.com/IL2HorusTeam/il2ds-log-parser',
     author='Alexander Oblovatniy',
     author_email='oblovatniy@gmail.com',
     packages=find_packages(exclude=["log_examples", ]),
     install_requires=[i.strip() for i in open("requirements.pip").readlines()],
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: Free for non-commercial use',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
