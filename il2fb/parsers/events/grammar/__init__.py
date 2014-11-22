b4e6d6720bbefcfdb2e825815015b1385e08b2e8
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-11-22 13:12:31 +0200

    define 'day_period' grammar

--- il2fb/parsers/events/grammar/__init__.py
+++ il2fb/parsers/events/grammar/__init__.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+
+from pyparsing import (
+    Combine, oneOf, Literal,
+)
+
+
+day_period = Combine(oneOf("A P") + 'M').setResultsName('day_period')
--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import unittest
+from pyparsing import ParseException
+
+from il2fb.parsers.events.grammar import (
+    day_period,
+)
+
+from .base import BaseTestCase
+
+
+class GrammarTestCase(BaseTestCase):
+
+    def test_day_period(self):
+        self.assertEqual(day_period.parseString('AM').day_period, 'AM')
+        self.assertEqual(day_period.parseString('PM').day_period, 'PM')
+
+        with self.assertRaises(ParseException):
+            day_period.parseString('ZZ')
