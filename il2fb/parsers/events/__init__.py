6df9abda012f4f081758928f3f6193646501fb48
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-11-22 11:21:28 +0200

    start global refactoring

--- il2fb/parsers/events/__init__.py
+++ il2fb/parsers/events/__init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from .parsers import default_evt_parser, parse_log_lines
+#  from .parsers import default_evt_parser, parse_log_lines
 
 
-__all__ = (
-    'parser', 'register', 'unregister', 'parse_evt', 'parse_log',
-)
+# __all__ = (
+#     'parser', 'register', 'unregister', 'parse_evt', 'parse_log',
+# )
 
-register = default_evt_parser.register
-unregister = default_evt_parser.unregister
-parse_evt = default_evt_parser
-parse_log = parse_log_lines
+# register = default_evt_parser.register
+# unregister = default_evt_parser.unregister
+# parse_evt = default_evt_parser
+# parse_log = parse_log_lines
