1a58d3fd7e9ee46a40154e64e4a1ccb0e7b2e4f9
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-11-27 21:02:47 +0200

    refactor events tests

--- tests/grammar/test_events.py
+++ tests/grammar/test_events.py
@@ -19,62 +19,63 @@ from ..base import BaseTestCase
 
 class EventsGrammarTestCase(BaseTestCase):
 
+    @staticmethod
+    def string_to_event(string, grammar):
+        return grammar.parseString(string).event
+
     def test_mission_is_playing(self):
         string = "[Sep 15, 2013 8:33:05 PM] Mission: path/PH.mis is Playing"
-        result = mission_is_playing.parseString(string).event
+        event = self.string_to_event(string, mission_is_playing)
 
-        self.assertIsInstance(result, MissionIsPlaying)
-        self.assertEqual(result.date, datetime.date(2013, 9, 15))
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
-        self.assertEqual(result.mission, "path/PH.mis")
+        self.assertIsInstance(event, MissionIsPlaying)
+        self.assertEqual(event.date, datetime.date(2013, 9, 15))
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.mission, "path/PH.mis")
 
     def test_mission_has_begun(self):
         string = "[8:33:05 PM] Mission BEGIN"
-        result = mission_has_begun.parseString(string).event
+        event = self.string_to_event(string, mission_has_begun)
 
-        self.assertIsInstance(result, MissionHasBegun)
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
+        self.assertIsInstance(event, MissionHasBegun)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
 
     def test_mission_has_ended(self):
         string = "[8:33:05 PM] Mission END"
-        result = mission_has_ended.parseString(string).event
+        event = self.string_to_event(string, mission_has_ended)
 
-        self.assertIsInstance(result, MissionHasEnded)
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
+        self.assertIsInstance(event, MissionHasEnded)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
 
     def test_mission_was_won(self):
         string = "[Sep 15, 2013 8:33:05 PM] Mission: RED WON"
-        result = mission_was_won.parseString(string).event
+        event = self.string_to_event(string, mission_was_won)
 
-        self.assertIsInstance(result, MissionWasWon)
-        self.assertEqual(result.date, datetime.date(2013, 9, 15))
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
-        self.assertEqual(result.belligerent, Belligerents.red)
+        self.assertIsInstance(event, MissionWasWon)
+        self.assertEqual(event.date, datetime.date(2013, 9, 15))
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.belligerent, Belligerents.red)
+
+    def test_target_state_has_changed(self):
+        string = "[8:33:05 PM] Target 3 Complete"
+        event = self.string_to_event(string, target_state_has_changed)
+
+        self.assertIsInstance(event, TargetStateHasChanged)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.target_index, 3)
+        self.assertEqual(event.state, TargetEndStates.COMPLETE)
+
+        string = "[8:33:05 PM] Target 4 Failed"
+        event = self.string_to_event(string, target_state_has_changed)
+
+        self.assertIsInstance(event, TargetStateHasChanged)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.target_index, 4)
+        self.assertEqual(event.state, TargetEndStates.FAILED)
 
     def test_user_has_connected(self):
         string = "[8:33:05 PM] User0 has connected"
-        result = user_has_connected.parseString(string).event
+        event = user_has_connected.parseString(string).event
+        event = self.string_to_event(string, user_has_connected)
 
-        self.assertIsInstance(result, UserHasConnected)
-        self.assertEqual(result.callsign, "User0")
-
-
-class TargetEndStateHasChangedTestCase(BaseTestCase):
-
-    def test_target_state_has_changed_to_complete(self):
-        string = "[8:33:05 PM] Target 3 Complete"
-        result = target_state_has_changed.parseString(string).event
-
-        self.assertIsInstance(result, TargetStateHasChanged)
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
-        self.assertEqual(result.target_index, 3)
-        self.assertEqual(result.state, TargetEndStates.COMPLETE)
-
-    def test_target_state_has_changed_to_failed(self):
-        string = "[8:33:05 PM] Target 4 Failed"
-        result = target_state_has_changed.parseString(string).event
-
-        self.assertIsInstance(result, TargetStateHasChanged)
-        self.assertEqual(result.time, datetime.time(20, 33, 5))
-        self.assertEqual(result.target_index, 4)
-        self.assertEqual(result.state, TargetEndStates.FAILED)
+        self.assertIsInstance(event, UserHasConnected)
+        self.assertEqual(event.callsign, "User0")
