dce61595c6f226dc24f64ca1b6d7485728be0ccc
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-12-13 10:17:15 +0200

    add exception handling

--- il2fb/parsers/events/__init__.py
+++ il2fb/parsers/events/__init__.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from .grammar.events import event
+import six
+import sys
+
+from .exceptions import EventParsingError
+from .grammar.events import event as _event_grammar
+from .structures.events import *
 
 
-parse_string = lambda x: event.parseString(x).event
+def parse_string(string):
+    try:
+        return _event_grammar.parseString(string).event
+    except Exception:
+        error_type, original_msg, traceback = sys.exc_info()
+        new_msg = ("{0} in string \"{1}\": {2}"
+                   .format(error_type.__name__, string, original_msg))
+        error = EventParsingError(new_msg)
+        six.reraise(EventParsingError, error, traceback)
---
+++ il2fb/parsers/events/exceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class EventParsingError(Exception):
+    """
+    Thrown when something bad has happened during parsing of event string.
+    """
