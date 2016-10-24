b522ba0e1bc39e1693cd8ab953bf064115e7aeec
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-24 17:19:01 +0300

    add AIAircraftCrewMemberWasKilledByStationaryUnit event

--- tests/test_events.py
+++ tests/test_events.py
@@ -2616,3 +2616,44 @@ class AIAircraftCrewMemberWasKilledTestCase(unittest.TestCase):
                 'verbose_name': "AI aircraft crew member was killed",
             }
         )
+
+
+class AIAircraftCrewMemberWasKilledByStationaryUnitTestCase(unittest.TestCase):
+
+    def test_from_s(self):
+        event = events.AIAircraftCrewMemberWasKilledByStationaryUnit.from_s(
+            "[8:33:05 PM] r01000(0) was killed by 0_Static at 100.0 200.99"
+        )
+        self.assertIsInstance(event, events.AIAircraftCrewMemberWasKilledByStationaryUnit)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.actor, actors.AIAircraftCrewMember("r0100", 0, 0))
+        self.assertEqual(event.attacker, actors.StationaryUnit("0_Static"))
+        self.assertEqual(event.pos, Point2D(100.0, 200.99))
+
+    def test_to_primitive(self):
+        event = events.AIAircraftCrewMemberWasKilledByStationaryUnit(
+            time=datetime.time(20, 33, 5),
+            actor=actors.AIAircraftCrewMember("r0100", 0, 0),
+            attacker=actors.StationaryUnit("0_Static"),
+            pos=Point2D(100.0, 200.99),
+        )
+        self.assertEqual(
+            event.to_primitive(),
+            {
+                'time': "20:33:05",
+                'actor': {
+                    'flight': "r0100",
+                    'aircraft': 0,
+                    'index': 0,
+                },
+                'attacker': {
+                    'id': "0_Static",
+                },
+                'pos': {
+                    'x': 100.0,
+                    'y': 200.99,
+                },
+                'name': "AIAircraftCrewMemberWasKilledByStationaryUnit",
+                'verbose_name': "AI aircraft crew member was killed by stationary unit",
+            }
+        )
