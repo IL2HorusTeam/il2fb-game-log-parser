6d5654febdbb853e369e2ffa6272f546fd1043ab
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-25 22:43:49 +0300

    add HumanAircraftCrewMemberWasKilledByAIAircraft event

--- tests/test_events.py
+++ tests/test_events.py
@@ -1773,6 +1773,48 @@ class HumanAircraftCrewMemberWasKilledByMovingUnitTestCase(unittest.TestCase):
         )
 
 
+class HumanAircraftCrewMemberWasKilledByAIAircraftTestCase(unittest.TestCase):
+
+    def test_from_s(self):
+        event = events.HumanAircraftCrewMemberWasKilledByAIAircraft.from_s(
+            "[8:33:05 PM] User0:Pe-8(0) was killed by r01000 at 100.0 200.99"
+        )
+        self.assertIsInstance(event, events.HumanAircraftCrewMemberWasKilledByAIAircraft)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.actor, actors.HumanAircraftCrewMember("User0", "Pe-8", 0))
+        self.assertEqual(event.attacker, actors.AIAircraft("r0100", 0))
+        self.assertEqual(event.pos, Point2D(100.0, 200.99))
+
+    def test_to_primitive(self):
+        event = events.HumanAircraftCrewMemberWasKilledByAIAircraft(
+            time=datetime.time(20, 33, 5),
+            actor=actors.HumanAircraftCrewMember("User0", "Pe-8", 0),
+            attacker=actors.AIAircraft("r0100", 0),
+            pos=Point2D(100.0, 200.99),
+        )
+        self.assertEqual(
+            event.to_primitive(),
+            {
+                'time': "20:33:05",
+                'actor': {
+                    'callsign': "User0",
+                    'aircraft': "Pe-8",
+                    'index': 0,
+                },
+                'attacker': {
+                    'flight': "r0100",
+                    'aircraft': 0,
+                },
+                'pos': {
+                    'x': 100.0,
+                    'y': 200.99,
+                },
+                'name': "HumanAircraftCrewMemberWasKilledByAIAircraft",
+                'verbose_name': "Human aircraft crew member was killed by AI aircraft",
+            }
+        )
+
+
 class BuildingWasDestroyedByHumanAircraftTestCase(unittest.TestCase):
 
     def test_from_s(self):
