f9cca3586fa690a9782c039515911cd55ecd465b
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-25 11:36:12 +0300

    add AIAircraftCrewMemberHasLanded event

--- il2fb/parsers/events/events.py
+++ il2fb/parsers/events/events.py
@@ -2184,3 +2184,29 @@ class AIAircraftCrewMemberHasBailedOut(Event):
         tx.ai_aircraft_crew_member_as_actor,
         tx.transform_pos,
     )
+
+
+class AIAircraftCrewMemberHasLanded(Event):
+    """
+    Example:
+
+        "[8:33:05 PM] r01000(0) successfully bailed out at 100.0 200.99"
+
+    """
+    __slots__ = ['time', 'actor', 'pos', ]
+
+    verbose_name = _("AI aircraft crew member has landed")
+    matcher = rx.matcher(
+        "{time}{actor}{s}successfully{s}bailed{s}out{pos}"
+        .format(
+            time=rx.TIME_GROUP_PREFIX,
+            actor=rx.AI_AIRCRAFT_CREW_MEMBER_ACTOR_GROUP,
+            pos=rx.POS_GROUP_SUFFIX,
+            s=rx.WHITESPACE,
+        )
+    )
+    transformers = (
+        tx.transform_time,
+        tx.ai_aircraft_crew_member_as_actor,
+        tx.transform_pos,
+    )
--- tests/test_events.py
+++ tests/test_events.py
@@ -3047,3 +3047,39 @@ class AIAircraftCrewMemberHasBailedOutTestCase(unittest.TestCase):
                 'verbose_name': "AI aircraft crew member has bailed out",
             }
         )
+
+
+class AIAircraftCrewMemberHasLandedTestCase(unittest.TestCase):
+
+    def test_from_s(self):
+        event = events.AIAircraftCrewMemberHasLanded.from_s(
+            "[8:33:05 PM] r01000(0) successfully bailed out at 100.0 200.99"
+        )
+        self.assertIsInstance(event, events.AIAircraftCrewMemberHasLanded)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.actor, actors.AIAircraftCrewMember("r0100", 0, 0))
+        self.assertEqual(event.pos, Point2D(100.0, 200.99))
+
+    def test_to_primitive(self):
+        event = events.AIAircraftCrewMemberHasLanded(
+            time=datetime.time(20, 33, 5),
+            actor=actors.AIAircraftCrewMember("r0100", 0, 0),
+            pos=Point2D(100.0, 200.99),
+        )
+        self.assertEqual(
+            event.to_primitive(),
+            {
+                'time': "20:33:05",
+                'actor': {
+                    'flight': "r0100",
+                    'aircraft': 0,
+                    'index': 0,
+                },
+                'pos': {
+                    'x': 100.0,
+                    'y': 200.99,
+                },
+                'name': "AIAircraftCrewMemberHasLanded",
+                'verbose_name': "AI aircraft crew member has landed",
+            }
+        )
