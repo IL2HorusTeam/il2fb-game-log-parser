96f0e10b345125db8c6c041d5dfa0dd9290ee5c5
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-27 17:32:11 +0300

    add AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMember event

--- tests/test_events.py
+++ tests/test_events.py
@@ -4819,6 +4819,48 @@ class AIAircraftCrewMemberParachuteWasDestroyedByStationaryUnitTestCase(unittest
         )
 
 
+class AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMemberTestCase(unittest.TestCase):
+
+    def test_from_s(self):
+        event = events.AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMember.from_s(
+            "[8:33:05 PM] r01000(0) has chute destroyed by 0_Chief0 at 100.0 200.99"
+        )
+        self.assertIsInstance(event, events.AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMember)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.actor, actors.AIAircraftCrewMember("r0100", 0, 0))
+        self.assertEqual(event.attacker, actors.MovingUnitMember("0_Chief", 0))
+        self.assertEqual(event.pos, Point2D(100.0, 200.99))
+
+    def test_to_primitive(self):
+        event = events.AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMember(
+            time=datetime.time(20, 33, 5),
+            actor=actors.AIAircraftCrewMember("r0100", 0, 0),
+            attacker=actors.MovingUnitMember("0_Chief", 0),
+            pos=Point2D(100.0, 200.99),
+        )
+        self.assertEqual(
+            event.to_primitive(),
+            {
+                'time': "20:33:05",
+                'actor': {
+                    'flight': "r0100",
+                    'aircraft': 0,
+                    'index': 0,
+                },
+                'attacker': {
+                    'id': "0_Chief",
+                    'index': 0,
+                },
+                'pos': {
+                    'x': 100.0,
+                    'y': 200.99,
+                },
+                'name': "AIAircraftCrewMemberParachuteWasDestroyedByMovingUnitMember",
+                'verbose_name': "AI aircraft crew member's parachute was destroyed by moving unit member",
+            }
+        )
+
+
 class AIAircraftCrewMemberParachuteWasDestroyedTestCase(unittest.TestCase):
 
     def test_from_s(self):
