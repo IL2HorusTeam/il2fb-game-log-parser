cdcbc08bf654635f17ff958d611f72953e9effe1
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-27 17:15:05 +0300

    add AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMember event

--- tests/test_events.py
+++ tests/test_events.py
@@ -4611,6 +4611,48 @@ class AIAircraftCrewMemberWasKilledInParachuteByStationaryUnitTestCase(unittest.
         )
 
 
+class AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMemberTestCase(unittest.TestCase):
+
+    def test_from_s(self):
+        event = events.AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMember.from_s(
+            "[8:33:05 PM] r01000(0) was killed in his chute by 0_Chief0 at 100.0 200.99"
+        )
+        self.assertIsInstance(event, events.AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMember)
+        self.assertEqual(event.time, datetime.time(20, 33, 5))
+        self.assertEqual(event.actor, actors.AIAircraftCrewMember("r0100", 0, 0))
+        self.assertEqual(event.attacker, actors.MovingUnitMember("0_Chief", 0))
+        self.assertEqual(event.pos, Point2D(100.0, 200.99))
+
+    def test_to_primitive(self):
+        event = events.AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMember(
+            time=datetime.time(20, 33, 5),
+            actor=actors.AIAircraftCrewMember("r0100", 0, 0),
+            attacker=actors.MovingUnitMember("0_Chief", 0),
+            pos=Point2D(100.0, 200.99),
+        )
+        self.assertEqual(
+            event.to_primitive(),
+            {
+                'time': "20:33:05",
+                'actor': {
+                    'flight': "r0100",
+                    'aircraft': 0,
+                    'index': 0,
+                },
+                'attacker': {
+                    'id': "0_Chief",
+                    'index': 0,
+                },
+                'pos': {
+                    'x': 100.0,
+                    'y': 200.99,
+                },
+                'name': "AIAircraftCrewMemberWasKilledInParachuteByMovingUnitMember",
+                'verbose_name': "AI aircraft crew member was killed in parachute by moving unit member",
+            }
+        )
+
+
 class AIAircraftCrewMemberParachuteWasDestroyedByAIAircraftTestCase(unittest.TestCase):
 
     def test_from_s(self):
