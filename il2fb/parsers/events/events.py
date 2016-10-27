3dd519780e694030c15922ec7cd84199d5415abc
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-10-27 17:29:43 +0300

    add AIAircraftCrewMemberParachuteWasDestroyedByStationaryUnit event

--- il2fb/parsers/events/events.py
+++ il2fb/parsers/events/events.py
@@ -3360,6 +3360,33 @@ class AIAircraftCrewMemberParachuteWasDestroyedByAIAircraft(Event):
     )
 
 
+class AIAircraftCrewMemberParachuteWasDestroyedByStationaryUnit(Event):
+    """
+    Example:
+
+        "[8:33:05 PM] r01000(0) has chute destroyed by 0_Static at 100.0 200.99"
+
+    """
+    __slots__ = ['time', 'actor', 'attacker', 'pos', ]
+
+    verbose_name = _("AI aircraft crew member's parachute was destroyed by stationary unit")
+    matcher = rx.matcher(
+        "{time}{actor}{s}has{s}chute{s}destroyed{s}by{s}{attacker}{pos}"
+        .format(
+            time=rx.TIME_GROUP_PREFIX,
+            actor=rx.AI_AIRCRAFT_CREW_MEMBER_ACTOR_GROUP,
+            attacker=rx.STATIONARY_UNIT_ATTACKER_GROUP,
+            pos=rx.POS_GROUP_SUFFIX,
+            s=rx.WHITESPACE,
+        )
+    )
+    transformers = (
+        tx.transform_time,
+        tx.ai_aircraft_crew_member_as_actor,
+        tx.stationary_unit_as_attacker,
+        tx.transform_pos,
+    )
+
 class AIAircraftCrewMemberParachuteWasDestroyed(Event):
     """
     Example:
