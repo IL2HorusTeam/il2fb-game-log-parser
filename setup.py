074369723bbe4080f9c5dd2c1e9d63a97dd8dd8b
Alexander Oblovatniy <oblovatniy@gmail.com>
2017-10-15 21:07:20 +0300

    Update version to 1.0.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ README = open(os.path.join(__here__, 'README.rst')).read()
 
 setup(
     name='il2fb-events-parser',
-    version='1.0.1',
+    version='1.0.2',
     description=(
         "Parse events from log produced by dedicated server of "
         "«IL-2 Forgotten Battles» flight simulator"
